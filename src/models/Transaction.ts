7a4bb1970e7f716527e1c497aa1f8d1d21bb2486
Ríad Oliveira <riad.oliveira@hotmail.com>
2020-11-11 02:35:23 -0300

    Cors and Models Update

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -4,7 +4,7 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  ManyToMany,
+  ManyToOne,
   JoinColumn,
 } from 'typeorm';
 
@@ -27,7 +27,7 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToMany(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
